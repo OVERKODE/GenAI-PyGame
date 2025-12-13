1888bc7e34e1d795d200488d8d6c2f09b16940d6
Alex Overkode <alexoverkode@hotmail.com>
2025-12-13 14:24:30 +0300

    Update 0.1.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="GenAI-PyGame",
+    name="genai_pygame",
     description='A helpful PyGame tool for intergrating Generative AI models into game projects.',
     author='Alex Khon',
     author_email='alexoverkode@hotmail.com',
-    version="0.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         'transformers'
