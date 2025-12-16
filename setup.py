9813925972662baf022eb4622a7685d0cd51888b
Alex Overkode <alexoverkode@hotmail.com>
2025-12-16 22:45:03 +0300

    Update 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='A helpful PyGame tool for intergrating Generative AI models into game projects.',
     author='Alex Khon',
     author_email='alexoverkode@hotmail.com',
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         'transformers'
